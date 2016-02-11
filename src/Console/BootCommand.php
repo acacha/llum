b9cc2c94663a586f779e50d9d849e656abe79f56
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2016-02-11 19:03:08 +0100

    solved some minor bugs and added boot command

---
+++ src/Console/BootCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Acacha\Llum\Console;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class BootCommand.
+ */
+class BootCommand extends LlumCommand
+{
+    /**
+     * Configure the command options.
+     */
+    protected function configure()
+    {
+        $this->ignoreValidationErrors();
+
+        $this->setName('boot')
+                ->setDescription('Execute all common first tasks in laravel project');
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->devtools($output);
+        $this->touchSqliteFile($output);
+        $this->configEnv($output);
+        $this->serve($output);
+    }
+}
