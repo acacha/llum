1011660ed94b0abcdb4f8d63112543d65c84997a
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2016-03-11 18:02:45 +0100

    Some cleaning working

--- tests/IlluminateBashScriptTest.php
+++ tests/IlluminateBashScriptTest.php
@@ -1,9 +1,5 @@
 <?php
 
-use Acacha\Llum\Console\SqliteTouchCommand;
-use Symfony\Component\Console\Application;
-use Symfony\Component\Console\Tester\CommandTester;
-
 /**
  * Class IlluminateBashScriptTest
  */
@@ -11,7 +7,6 @@ class IlluminateBashScriptTest extends \PHPUnit_Framework_TestCase
 {
 
     /**
-     * @group failing
      * test IlluminateBashScriptTest
      */
     public function testExecute()
