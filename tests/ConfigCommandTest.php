3023e6be12b87b9cd29a4f9891e0ee4e7e4d9ce6
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2016-03-12 12:14:41 +0100

    New tests addes

---
+++ tests/ConfigCommandTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Acacha\Llum\Tests;
+
+use Acacha\Llum\Console\ConfigCommand;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * Class ConfigCommandTest
+ */
+class ConfigCommandTest extends LlumCommandTest
+{
+
+    protected function setUp()
+    {
+        passthru('./test_setup.sh');
+    }
+
+    protected function tearDown()
+    {
+        passthru('./test_teardown.sh');
+    }
+
+    /**
+     * @group failing
+     * test DevToolsCommand
+     */
+    public function testExecute()
+    {
+        $application = new Application();
+        $application->add(new ConfigCommand());
+
+        $command = $application->find('config');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array(
+            'command' => $command->getName(),
+            )
+        );
+
+        $this->assertTrue(
+            $this->fileHasContent('/config/app.php','#llum_providers')
+        );
+        $this->assertTrue(
+            $this->fileHasContent('/config/app.php','#llum_aliases')
+        );
+
+    }
+
+
+}
---
+++ tests/LlumCommandTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Acacha\Llum\Tests;
+
+/**
+ * Class ConfigCommandTest
+ */
+abstract class LlumCommandTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Check file config/app.php has content.
+     *
+     * @param $content
+     * @return bool
+     */
+    protected function laravelConfigFileHasContent($content)
+    {
+        return $this->fileHasContent('/config/app.php', $content);
+    }
+
+    /**
+     * Check if file has an specific content.
+     *
+     * @param $file
+     * @param $content
+     * @return bool
+     */
+    protected function fileHasContent($file, $content)
+    {
+        return strpos(file_get_contents(getcwd() . $file), $content) != false;
+    }
+}
