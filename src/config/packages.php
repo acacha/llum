c83ef0938abb30a1f6d9150957bc91f405415967
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2016-09-16 14:14:30 +0200

    AdminLTE now is installed by default using artisan adminlte-laravel:publish commands insted of vendor:publish

--- src/config/packages.php
+++ src/config/packages.php
@@ -15,6 +15,13 @@ return [
         'name' => 'acacha/admin-lte-template-laravel',
         'providers' => ['Acacha\AdminLTETemplateLaravel\Providers\AdminLTETemplateServiceProvider::class'],
         'aliases' => ['AdminLTE' => 'Acacha\AdminLTETemplateLaravel\Facades\AdminLTE::class'],
+        'after' => 'php artisan adminlte-laravel:publish',
+    ],
+
+    'AdminLTEVendorPublish' => [
+        'name' => 'acacha/admin-lte-template-laravel',
+        'providers' => ['Acacha\AdminLTETemplateLaravel\Providers\AdminLTETemplateServiceProvider::class'],
+        'aliases' => ['AdminLTE' => 'Acacha\AdminLTETemplateLaravel\Facades\AdminLTE::class'],
         'after' => 'php artisan vendor:publish --tag=adminlte --force',
     ],
 
