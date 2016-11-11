4dab0be1493ab7bf17a82c9de45aae45c023e66e
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2016-11-11 12:59:25 +0100

    Solved bug https://github.com/acacha/llum/issues/20

--- src/Console/BootCommand.php
+++ src/Console/BootCommand.php
@@ -7,6 +7,8 @@ use Acacha\Llum\Traits\Migrate;
 use Acacha\Llum\Traits\Serve;
 use Acacha\Llum\Traits\SqliteEnv;
 use Acacha\Llum\Traits\TouchSqliteFile;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Class BootCommand.
@@ -36,10 +38,14 @@ class BootCommand extends LlumCommand
      */
     protected $method = 'boot';
 
+
     /**
      * Executes boot command.
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
      */
-    protected function boot()
+    protected function boot(InputInterface $input, OutputInterface $output)
     {
         if ($this->devtools() == -1) {
             return;
@@ -47,6 +53,6 @@ class BootCommand extends LlumCommand
         $this->touchSqliteFile();
         $this->sqliteEnv();
         $this->migrate();
-        $this->serve();
+        $this->serve($input, $output);
     }
 }
